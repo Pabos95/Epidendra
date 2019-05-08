5d4bda206b23a459270dfd866815aaa35ad553ec
Pabos95 <pabloagc95@gmail.com>
2019-05-08 12:21:37 -0600

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,255 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html><!-- InstanceBegin template="/Templates/adentro2012.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta property="og:title" content="The global orchid taxonomic netwotrk" />
+<meta property="og:type" content="university" />
+<meta property="og:url" content="http://www.epidendra.org" />
+<meta property="og:image" content="http://www.epidendra.org/fotos/2012/adentro-bottom.jpg" />
+<meta property="og:site_name" content="EPIDENDRA.org" />
+<meta property="fb:admins" content="100001609783458" />
+<link href="tools.css" type="text/css" rel="STYLESHEET">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>THE GLOBAL ORCHID TAXONOMIC NETWORK</title>
+<!-- InstanceEndEditable --><meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+body {
+	background-image:  url(fotos/2012/adentroBG.jpg);
+}
+.style1 {color: #E7E7E7}
+.style2 {color: #0000FF}
+.style4 {text-decoration:none; color:#7d715e; cursor: hand; ; font-family: Verdana, Arial, Helvetica, sans-serif; font-size: 14px}
+-->
+</style>
+<!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1531358-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+</head>
+
+<body bgproperties="fixed" leftmargin="0" topmargin="0">
+<!-- DO NOT MOVE! The following AllWebMenus linking code section must always be placed right AFTER the BODY tag-->
+<!-- ******** BEGIN ALLWEBMENUS CODE FOR menu ******** -->
+<script type="text/javascript">var MenuLinkedBy='AllWebMenus [4]',awmMenuName='menu',awmBN='DW';awmAltUrl='';</script>
+<script charset= "UTF-8" src="menu.js" type="text/javascript"></script>
+<script type= "text/javascript">awmBuildMenu();</script>
+<!-- ******** END ALLWEBMENUS CODE FOR menu ******** -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<table width="100%"  border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td align="center" valign="top"><table width="1000" align="center" border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td valign="top"><img src="fotos/10x10.gif" width="100" height="15"></td>
+        </tr>
+        <tr>
+          <td valign="top" background="fotos/2012/adentro-top22.jpg"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+              <tr valign="top">
+                <td rowspan="2"><img src="fotos/10x10.gif" width="2" height="141"></td>
+                <td><img src="fotos/10x10.gif" width="1010" height="2"></td>
+              </tr>
+              <tr>
+                <td valign="top"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+					<!-- InstanceBeginEditable name="BANNER" -->
+                    <tr valign="top">
+                      <td width="60%" align="right"><a name="top"></a><a href="http://www.epidendra.org"><img src="fotos/10x10.gif" alt="EPIDENDRA:  THE GLOBAL ORCHID TAXONOMIC NETWORK" width="200" height="100" border="0"></a></td>
+                      <td width="40%" align="right"><img src="fotos/10x10.gif" width="345" height="95" border="0" usemap="#Map"></td>
+                    </tr>
+                    <tr valign="top">
+                      <td valign="bottom"><a name="awmAnchor-menu" id="awmAnchor-menu"></a></td>
+                      <td><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+                          <tr>
+                            <td colspan="2"><span class="titulo19"><img src="fotos/10x10.gif" width="200" height="5"></span></td>
+                          </tr>
+                          <tr>
+                            <td width="95%"><form id="searchbox_002957403017895698234:ulverbyi5nm" action="http://www.google.com/search" target="_blank">
+                                <div align="right">
+                                  <input type="hidden" name="cx" value="002957403017895698234:ulverbyi5nm" />
+                                  <input type="hidden" name="cof" value="FORID:0" />
+                                  <input name="q" type="text" class="formTEXTFIELD" title="... Search Names" value="... Search Names" size="27" onFocus="this.value=''" />
+                                  <input name="sa" type="submit" class="formBOTTOM" value="GO" />
+                                </div>
+                            </form></td>
+                            <td width="5%" align="right" valign="top" class="titulo19"><div align="right"><img src="fotos/10x10.gif" width="27" height="10"></div></td>
+                          </tr>
+                      </table></td>
+                    </tr>
+                </table><!-- InstanceEndEditable --></td>
+              </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td valign="top" background="fotos/2012/adentro-middleBG.jpg" bgcolor="#FFFFFF"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td width="15%"><img src="fotos/10x10.gif" width="152" height="2"></td>
+                <td width="2%"><img src="fotos/10x10.gif" width="28" height="2"></td>
+                <td width="81%"><img src="fotos/10x10.gif" width="810" height="2"></td>
+                <td width="2%"><img src="fotos/10x10.gif" width="23" height="2"></td>
+              </tr>
+              <tr>
+				  <!-- InstanceBeginEditable name="enlaces" -->
+                <td align="left" valign="top"><table width="100%"  border="0" cellspacing="5" cellpadding="0">
+                  <tr valign="top">
+                    <td colspan="2" valign="middle"><img src="fotos/10x10.gif" width="144" height="20"></td>
+                    </tr>
+                  <tr valign="top">
+                    <td width="9%" valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td width="91%" valign="middle"><a href="HTTP://WWW.EPIDENDRA.ORG" class="aESPEC22">HOME</a></td>
+                  </tr>
+                  <tr valign="top">
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="taxones/index.html" class="aESPEC22">LIST OF SPECIES </a></td>
+                    </tr>
+                  <tr valign="top">
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="LITERATURE/index.htm" class="aESPEC22">LITERATURE</a></td>
+                    </tr>
+                  <tr>
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="floras/INDEX.htm" class="aESPEC22">FLORAS</a></td>
+                    </tr>
+                  <tr valign="top">
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="parquesnacionales/index.htm" class="aESPEC22">NATIONAL PARKS </a></td>
+                    </tr>
+                  <tr valign="top">
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="ORCHIDhistory/INDEX.htm" class="aESPEC22">ORCHID HISTORY</a></td>
+                    </tr>
+                  <tr valign="top">
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="NEWrecords/index.htm" class="aESPEC22">NEW RECORDS </a></td>
+                    </tr>
+                  <tr valign="top">
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="plates/index.htm" class="aESPEC22">COLLECTABLE PLATES</a></td>
+                    </tr>
+                  <tr valign="top">
+                    <td colspan="2" bgcolor="#CCCCCC"><img src="fotos/10x10.gif" width="10" height="1"></td>
+                    </tr>
+                  <tr>
+                    <td valign="middle"><img src="fotos/2012/1bullet.jpg" width="9" height="11"></td>
+                    <td valign="middle"><a href="Herbarium/index.htm" class="aESPEC22">HERBARIUM</a></td>
+                  </tr>
+					<!-- InstanceEndEditable -->
+                </table>                
+                <p>&nbsp;</p>
+                </td>
+                <td valign="top">&nbsp;</td>
+                <td valign="top"> <!-- InstanceBeginEditable name="CONTENIDOS" -->
+                  <table width="100%"  border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td valign="top" class="titulo1"><p>&nbsp;</p>
+                      <p>404. CONTENIDO NO ENCONTRADO </p></td>
+                    </tr>
+                    <tr>
+                      <td valign="top"><img src="fotos/10x10.gif" width="10" height="10"></td>
+                    </tr>
+                    <tr>
+                      <td valign="top"><p class="cuerpotxt"><strong>Lo sentimos mucho, pero el destino que busca no est&aacute; disponible en este momento o no fue encontrado en el servidor web de EPIDENDRA. Le sugerimos optar por alguno de los siguientes recursos:</strong></p>
+                        <table width="100%"  border="0" cellspacing="6" cellpadding="0">
+                          <tr>
+                            <td width="2%" valign="bottom"><span class="cuerpotxt"><img src="fotos/2012/1bullet.jpg" width="11" height="11"></span></td>
+                            <td colspan="2" valign="top" class="cuerpotxt">I<span class="cuerpotxt">r a la <a href="http://www.epidendra.org%20"><strong>P&aacute;gina Principal</strong></a></span></td>
+                          </tr>
+                          <tr>
+                            <td valign="bottom"><span class="cuerpotxt"><img src="fotos/2012/1bullet.jpg" width="11" height="11"></span></td>
+                            <td width="80%" valign="top" class="cuerpotxt">Realizar una B&uacute;squeda en nuestro Motor de B&uacute;squeda</td>
+                            <td width="18%" valign="top" class="cuerpotxt">&nbsp;</td>
+                          </tr>
+                          <tr>
+                            <td valign="bottom"><span class="cuerpotxt"><img src="fotos/2012/1bullet.jpg" width="11" height="11"></span></td>
+                            <td colspan="2" valign="top" class="cuerpotxt">Navegar por el <a href="taxones/index.html"><strong>listado de especies</strong></a> de EPIDENDRA </td>
+                          </tr>
+                          <tr>
+                            <td valign="bottom"><span class="cuerpotxt"><img src="fotos/2012/1bullet.jpg" width="11" height="11"></span></td>
+                            <td colspan="2" valign="top" class="cuerpotxt">Revisar los t&iacute;tulos de <a href="LITERATURE/index.htm"><strong>Literatura</strong></a> que tenemos disponibles en PDF </td>
+                          </tr>
+                          <tr>
+                            <td valign="bottom"><span class="cuerpotxt"><img src="fotos/2012/1bullet.jpg" width="11" height="11"></span></td>
+                            <td colspan="2" valign="top" class="cuerpotxt">Ver las <a href="plates/index.htm"><strong>l&aacute;minas de Orqu&iacute;deas</strong></a> </td>
+                          </tr>
+                          <tr>
+                            <td valign="bottom"><span class="cuerpotxt"><img src="fotos/2012/1bullet.jpg" width="11" height="11"></span></td>
+                            <td colspan="2" valign="top" class="cuerpotxt">Enviar un correo electr&oacute;nico al <a href="mailto:admin@epidendra.org"><strong>Webmaster</strong></a> </td>
+                          </tr>
+                        </table>                        
+                        <div align="right"></div>                        
+                        <p class="cuerpotxt">&nbsp;</p>
+                        <p class="cuerpotxt"><strong>Gracias por su comprensi&oacute;n. </strong></p></td>
+                    </tr>
+                  </table>
+                <!-- InstanceEndEditable -->
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td align="left" valign="top">&nbsp;</td>
+                <td valign="top">&nbsp;</td>
+                <td valign="top"><div align="center">
+                  <table width="100%"  border="0" cellspacing="0" cellpadding="6">
+                    <tr>
+                      <td><div class="fb-like" data-href="http://www.epidendra.org/" data-send="true" data-width="450" data-show-faces="true" data-action="recommend" data-font="arial"></div></td>
+                    </tr>
+                  </table>
+                </div></td>
+                <td>&nbsp;</td>
+              </tr>
+          </table></td>
+        </tr>
+        <tr>
+          <td valign="top" bgcolor="#FFFFFF"><img src="fotos/2012/adentro-bottom.jpg" width="1012" height="104" border="0" usemap="#Map2"></td>
+        </tr>
+      </table>
+        <p class="menuX"> &copy; 2018 Jard&iacute;n Bot&aacute;nico Lankester . Universidad de Costa Rica - Tel. 2511-7931 <span class="pie style1">....</span> Desarollo <a href="http%3E//www.tools.co.cr" target="_blank" class="menuX style2">TOOLS</a> </p></td>
+  </tr>
+</table>
+<map name="Map">
+  <area shape="rect" coords="257,53,331,84" href="contacto.html" alt="CONTACTO">
+  <area shape="rect" coords="276,-3,345,20" href="http://www.epidendra.org" alt="HOME">
+  <area shape="rect" coords="276,19,343,35" href="links.html" alt="LINKS">
+  <area shape="rect" coords="265,35,345,53" href="about.html">
+</map>
+<map name="Map2">
+  <area shape="rect" coords="912,5,1009,91" href="http://www.jbl.ucr.ac.cr" target="_blank" alt="LANKESTER BOTANICAL GARDEN WEBSITE">
+  <area shape="rect" coords="815,4,908,95" href="http://www.ucr.ac.cr" target="_blank" alt="UNIVERSIDAD DE COSTA RICA">
+</map>
+</body>
+<!-- InstanceEnd --></html>
